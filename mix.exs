02794aa3cd36e26381a102441b0ed52c62d7053c
red-jade <katagorikal@gmail.com>
2024-07-13 13:16:50 +0700

    Roll version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exa.Space.MixProject do
     [
       app: :exa_space,
       name: "Exa Space",
-      version: "0.1.2",
+      version: "0.1.4",
       elixir: "~> 1.15",
       erlc_options: [:verbose, :report_errors, :report_warnings, :export_all],
       start_permanent: Mix.env() == :prod,
@@ -35,7 +35,7 @@ defmodule Exa.Space.MixProject do
     [
       # runtime code dependencies ----------
 
-      {:exa, git: "https://github.com/red-jade/exa_core.git", tag: "v0.1.2"},
+      {:exa, git: "https://github.com/red-jade/exa_core.git", tag: "v0.1.4"},
 
       # building, documenting, testing ----------
 
