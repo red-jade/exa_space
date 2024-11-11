e9fe361f80e3bc939c3c572019d961eab29d8d0f
red-jade <katagorikal@gmail.com>
2024-11-11 15:53:35 +0700

    Deps data

---
+++ deps.ex
@@ -0,0 +1,6 @@
+[
+  {:exa_core,
+   [git: "https://github.com/red-jade/exa_core.git", tag: "v0.3.1", app: false]},
+  {:dialyxir, "~> 1.0", [only: [:dev, :test], runtime: false]},
+  {:ex_doc, "~> 0.30", [only: [:dev, :test], runtime: false]}
+]
