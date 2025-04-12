8a33ad42087eebc0c5270820ce7b74df8e116ff5
red-jade <katagorikal@gmail.com>
2025-04-12 15:18:11 +0700

    Roll version

--- deps.ex
+++ deps.ex
@@ -1,5 +1,5 @@
 [
-  {:exa_core, [git: "https://github.com/red-jade/exa_core.git", tag: "v0.3.2"]},
+  {:exa_core, [git: "https://github.com/red-jade/exa_core.git", tag: "v0.3.3"]},
   {:dialyxir, "~> 1.0", [only: [:dev, :test], runtime: false]},
   {:ex_doc, "~> 0.30", [only: [:dev, :test], runtime: false]}
 ]
